b0943d8e0d4dc5a3527aa85339557bbe5a1a8344
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-03-09 16:58:27 +0100

    Add custom not found page

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,9 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\IndexController;
 
 Route::get('/', [IndexController::class, 'index'])->name('home');
+
 Route::get('/article/{id}', [ArticleController::class, 'show'])->name('article.details');
+
+Route::fallback(function () {
+    return view('errors.not-found');
+});
