b5195a4be323e57acee192ee30d22095e5c26005
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-03-09 16:06:13 +0100

    Add show method

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,4 +7,8 @@ class IndexController extends Controller
     {
         return view('welcome', ["name" => "Mamitiana"]);
     }
+
+    public function show($id): string{
+        return "Article portant l'identifiant $id.";
+    }
 }
