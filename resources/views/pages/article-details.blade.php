720f2e4e84996f52c878a415828a2a025dca1241
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-03-10 10:09:01 +0100

    Add id to title

--- resources/views/pages/article-details.blade.php
+++ resources/views/pages/article-details.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Détails de l\'article')
+@section('title', 'Détails de l\'article ' . $id)
 @section('content')
     <h2>Article portant l'identifiant {{ $id }}</h2>
 @endsection
