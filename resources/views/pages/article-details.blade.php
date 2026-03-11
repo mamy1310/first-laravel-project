ab41a06858d1526cb25738752e8bd887f5b37281
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-03-11 12:53:51 +0100

    Refactor

--- resources/views/pages/article-details.blade.php
+++ resources/views/pages/article-details.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 @section('title', 'Détails de l\'article ' . $article->id)
 @section('content')
-    <x-article :id="$article['id']" :titre="$article['titre']" :description="$article['description']"/>
+    <x-article :id="$article->id" :titre="$article->titre" :description="$article->description"/>
 @endsection
